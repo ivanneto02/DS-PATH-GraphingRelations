a8d1c7a5c4bc90fd67f875776bc10bea7087e1de
ivanneto02 <ivan.a.neto@hotmail.com>
2022-07-14 06:26:28 -0700

    Temporary main
    Right now main function as the mechanism which will later work
    in the background within RXNormVisualize

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from tkinter import colorchooser
 import graphviz
 import mysql
 
 from config import *
 
 import src.RXNormVisualize as viz
+
+from datetime import datetime
 
 def main():
     # connect to database, return connection
@@ -13,19 +16,22 @@ def main():
         user = mysql_username,
         password = mysql_password)
 
-    queries = viz.get_queries(CUIs)
-    results = viz.send_queries(queries, connection)
+    relations_queries = viz.get_relations_queries(CUIs)
+    relations_results = viz.send_relations_queries(relations_queries, connection)
+    get_strings_dic = viz.get_strings_dic(relations_results, connection)
 
-    # # Printing out the results
-    # for i in range(len(results)):
-    #     print(f"Printing result {i}...")
-    #     for row in results[i][:LIMIT_ROWS]:
-    #         print(row)
+    stop_letters = None
 
-    for i in range(len(results)):
-        gra = graphviz.Digraph()
-        gra.node(i, results[i][0])
-        # Create graph
+    for i in range(len(relations_results)):
+        print(f"> Creating graph {i+1}...")
+        gra = graphviz.Digraph(format="png", node_attr={"colorscheme" : "gnbu9", "style" : "filled"})
+        gra.node(str(i), str(relations_results[i][0][0])+'\n'+str(get_strings_dic[relations_results[i][0][0]])[:stop_letters])
+        edges = []
+        for j in range(len(relations_results[i])):
+            gra.node(f"{i}{j}", str(relations_results[i][j][2])+'\n'+str(get_strings_dic[relations_results[i][j][2]])[:stop_letters])
+            gra.edge(str(i), f"{i}{j}", label=str(relations_results[i][j][1]))
+        gra = gra.unflatten(stagger=10)
+        gra.render(image_save_path + f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{relations_results[i][0][0]}')
 
     # will disconnect the connection
     viz.disconnect(connection)
