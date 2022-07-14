c367d9a6b773d35c0f939ae6d44af1aa907b71da
ivanneto02 <ivan.a.neto@hotmail.com>
2022-07-14 09:25:17 -0700

    visualize.py now holds the visualization code
    Before, main.py held that code. But now the visualization code is within
    visualize.py so that the user now only has to call the visualize(CUIs :
    List) function to output out all of the images from the visualization

--- src/RXNormVisualize/__init__.py
+++ src/RXNormVisualize/__init__.py
@@ -3,3 +3,4 @@ from .disconnect import disconnect
 from .get_relations_queries import get_relations_queries
 from .send_relations_queries import send_relations_queries
 from .get_strings_dic import get_strings_dic
+from .visualize import visualize
--- src/RXNormVisualize/visualize.py
+++ src/RXNormVisualize/visualize.py
@@ -1,7 +1,43 @@
-def graph_results(results=[]):
-    if len(results) == 0:
-        print("Nothing to graph.")
+from config import *
+from .connect import connect
+from .get_relations_queries import get_relations_queries
+from .send_relations_queries import send_relations_queries
+from .get_strings_dic import get_strings_dic
+from .disconnect import disconnect
+import graphviz
+from datetime import datetime
+
+def visualize(CUIs=[]):
+    print("> Starting the visualization process")
+    if len(CUIs) == 0:
+        print("> - Nothing to graph.")
         return
+
+    # connect to database, return connection
+    connection = connect(
+        host = mysql_host,
+        database = mysql_database,
+        user = mysql_username,
+        password = mysql_password)
+
+    relations_queries = get_relations_queries(CUIs)
+    relations_results = send_relations_queries(relations_queries, connection)
+    strings_dic = get_strings_dic(relations_results, connection)
+
+    stop_letters = None
+
+    for i in range(len(relations_results)):
+        print(f"> Creating graph {i+1}...")
+        gra = graphviz.Digraph(format="png", node_attr={"colorscheme" : "gnbu9", "style" : "filled"})
+        gra.node(str(i), str(relations_results[i][0][0])+'\n'+str(strings_dic[relations_results[i][0][0]])[:stop_letters])
+        for j in range(len(relations_results[i])):
+            gra.node(f"{i}{j}", str(relations_results[i][j][2])+'\n'+str(strings_dic[relations_results[i][j][2]])[:stop_letters])
+            gra.edge(str(i), f"{i}{j}", label=str(relations_results[i][j][1]))
+        gra = gra.unflatten(stagger=10)
+        gra.render(image_save_path + f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{relations_results[i][0][0]}')
+
+    # will disconnect the connection
+    disconnect(connection)
 
 if __name__ == "__main__":
     print("Running send_queries module of RXNormVisualize package.")
